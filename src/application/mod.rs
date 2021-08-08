f603c2e73e09ee1f7065c173e46fabe55e1e598b
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-08-08 13:48:17 +0900

    Update mod.rs

--- src/application/mod.rs
+++ src/application/mod.rs
@@ -10,26 +10,32 @@ use actix_web::{get, web, App, HttpServer, Responder};
 
 #[get("/{id}/{name}/index.html")]
 async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
-    format!("Hello {}! id:{}", name, id)
+	format!("Hello {}! id:{}", name, id)
 }
 
+// curl http://127.0.0.1:8080/1/masaru/index.html
 impl Application {
-    pub fn new() -> Application {
-        return Application {};
-    }
+	/// `Application` の新しインスタンスを作成します。
+	///
+	/// ## Returns
+	// `Application` の新しいインスタンス
+	pub fn new() -> Application {
+		return Application {};
+	}
 
-    pub async fn run(&mut self) -> std::result::Result<(), std::io::Error> {
-        // データベース初期化
-        db::init()?;
+	/// ウェブサーバーを起動します。
+	pub async fn run(&mut self) -> std::result::Result<(), Box<dyn std::error::Error>> {
+		// データベース初期化
+		db::init()?;
 
-        // ウェブサーバーを起動
-        let _context = HttpServer::new(|| App::new().service(index))
-            .bind("127.0.0.1:8080")?
-            .run()
-            .await;
+		// ウェブサーバーを起動
+		let _context = HttpServer::new(|| App::new().service(index))
+			.bind("127.0.0.1:8080")?
+			.run()
+			.await;
 
-        println!("Ok.");
+		println!("Ok.");
 
-        return Ok(());
-    }
+		return Ok(());
+	}
 }
